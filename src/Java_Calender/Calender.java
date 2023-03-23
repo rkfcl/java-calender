08a87a68acf07b1377a2aaab037e6fb6917890ab
user <user@DESKTOP-BGPO6F5>
2023-03-24 02:04:34 +0900

    윤년 판별 구현

--- src/Java_Calender/Calender.java
+++ src/Java_Calender/Calender.java
@@ -3,11 +3,22 @@ package Java_Calender;
 import java.io.IOException;
 
 public class Calender {
+	private final static int[] Leap_Year_MaxDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private final static int[] MaxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private final static String[] Days = { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
 
-	public int GetMaxDays(int Month) {
+	public int GetMaxDays(int year, int Month) {
+		if (leapYear(year, Month)) {
+			return (Leap_Year_MaxDays[Month - 1]);
+		}
 		return (MaxDays[Month - 1]);
+	}
+
+	public boolean leapYear(int year, int Month) {
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+			return true;
+		}
+		return false;
 	}
 
 	public void printCalender(int year, int month) {
@@ -17,7 +28,7 @@ public class Calender {
 		}
 		System.out.println();
 		System.out.println("----------------------------");
-		for (int i = 1; i <= GetMaxDays(month); i++) {
+		for (int i = 1; i <= GetMaxDays(year, month); i++) {
 			System.out.printf("%4d", i);
 			if (i % 7 == 0) {
 				System.out.println();

a10dc3c8c2d31d0924ff8d58bb0855d3885408ba
user <user@DESKTOP-BGPO6F5>
2023-03-24 01:39:29 +0900

    달력 출력 클래스 만듬

--- src/Java_Calender/Calender.java
+++ src/Java_Calender/Calender.java
@@ -1,9 +1,32 @@
 package Java_Calender;
 
-public class Calender {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		System.out.println("Hello calender");
+public class Calender {
+	private final static int[] MaxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private final static String[] Days = { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
+
+	public int GetMaxDays(int Month) {
+		return (MaxDays[Month - 1]);
+	}
+
+	public void printCalender(int year, int month) {
+		System.out.printf("	 %3d³â %3d¿ù\n", year, month);
+		for (int i = 0; i < Days.length; i++) {
+			System.out.printf("%4s", Days[i]);
+		}
+		System.out.println();
+		System.out.println("----------------------------");
+		for (int i = 1; i <= GetMaxDays(month); i++) {
+			System.out.printf("%4d", i);
+			if (i % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) throws IOException {
 
 	}
 
---
+++ src/Java_Calender/prompt.java
@@ -0,0 +1,39 @@
+package Java_Calender;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class prompt {
+	public void runPrompt() throws IOException {
+		Calender cal = new Calender();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while (true) {
+			
+			System.out.println("년도와 달을 입력하세요");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int Y = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			if (M == -1) {
+				break;
+			}
+			if (M > 12) {
+				continue;
+			}
+			cal.printCalender(Y, M);
+
+		}
+		br.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		prompt p = new prompt();
+		p.runPrompt();
+
+	}
+
+}

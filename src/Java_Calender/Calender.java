f5268e605b3d8981b187ebba5126ba542e289b6e
user <user@DESKTOP-BGPO6F5>
2023-03-27 00:31:18 +0900

    일정 관련 기능 추가

--- src/Java_Calender/Calender.java
+++ src/Java_Calender/Calender.java
@@ -3,17 +3,31 @@ package Java_Calender;
 import java.io.IOException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashMap;
 
 public class Calender {
+
 	private final static int[] Leap_Year_MaxDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private final static int[] MaxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private final static String[] Days = { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
-	public int GetDayOfWeek(int year,int Month)
-	{
+
+	private HashMap<Date, String> planmap = new HashMap<Date, String>();
+
+	public void PlanMap(Date date, String str) {
+		planmap.put(date, str);
+	}
+
+	public void cmdPlanMap(Date date) {
+		System.out.println(planmap.get(date));
+	}
+
+	public int GetDayOfWeek(int year, int Month) {
 		LocalDate date = LocalDate.of(year, Month, 1);
 		DayOfWeek dayofweek = date.getDayOfWeek();
 		return dayofweek.getValue();
 	}
+
 	public int GetMaxDays(int year, int Month) {
 		if (leapYear(year, Month)) {
 			return (Leap_Year_MaxDays[Month - 1]);
@@ -35,16 +49,16 @@ public class Calender {
 		}
 		System.out.println();
 		System.out.println("----------------------------");
-		for(int i=0;i<GetDayOfWeek(year, month);i++)
-		{
+		for (int i = 0; i < GetDayOfWeek(year, month); i++) {
 			System.out.printf("    ");
 		}
 		for (int i = 1; i <= GetMaxDays(year, month); i++) {
 			System.out.printf("%4d", i);
-			if ((i+GetDayOfWeek(year, month)) % 7 == 0) {
+			if ((i + GetDayOfWeek(year, month)) % 7 == 0) {
 				System.out.println();
 			}
 		}
+		System.out.println();
 		System.out.println();
 	}
 
--- src/Java_Calender/prompt.java
+++ src/Java_Calender/prompt.java
@@ -3,31 +3,142 @@ package Java_Calender;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.StringTokenizer;
 
 public class prompt {
-	public void runPrompt() throws IOException {
+	public void runPrompt() {
 		Calender cal = new Calender();
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in, "euc-kr"));
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		while (true) {
-			
-			System.out.println("년도와 달을 입력하세요");
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int Y = Integer.parseInt(st.nextToken());
-			if (Y == -1) {
-				break;
-			}
-			int M = Integer.parseInt(st.nextToken());
-			
-			if (M > 12) {
+
+			System.out.println("+----------------------+");
+			System.out.println("| 1. 일정 등록");
+			System.out.println("| 2. 일정 검색");
+			System.out.println("| 3. 달력 보기");
+			System.out.println("| 4. 도움말");
+			System.out.println("| 5. 종료");
+			System.out.println("+----------------------+");
+			try {
+				int N = Integer.parseInt(br.readLine());
+				switch (N) {
+				case 1:
+					ScheduleRegistration(br, cal);
+					break;
+				case 2:
+					cmdplan(br, cal);
+					break;
+				case 3:
+					cmdcal(br, cal);
+					break;
+				case 4:
+
+					break;
+				case 5:
+					br.close();
+					return;
+				default:
+					System.out.println("잘못된 값 입력");
+					continue;
+				}
+			} catch (NumberFormatException | IOException e) {
+				System.out.println("잘못된 값 입력!");
 				continue;
 			}
-			cal.printCalender(Y, M);
 
 		}
-		br.close();
+	}
+
+	public void cmdcal(BufferedReader br, Calender cal) {
+		while (true) {
+			System.out.println("년도와 달을 입력하세요(exit=-1)");
+			try {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int Y = Integer.parseInt(st.nextToken());
+				if (Y == -1) {
+					break;
+				}
+				int M = Integer.parseInt(st.nextToken());
+
+				if (M > 12 || M < 1) {
+					System.out.println("잘못된 날짜 입력");
+					continue;
+				}
+				cal.printCalender(Y, M);
+				break;
+			} catch (IOException e) {
+				continue;
+			}
+		}
+	}
+
+	public void ScheduleRegistration(BufferedReader br, Calender cal) {
+		while (true) {
+			Date Date = null;
+			String schedule = null;
+			System.out.println("년도와 달 날짜를 입력 하시오.");
+			try {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int YY = Integer.parseInt(st.nextToken());
+				int MM = Integer.parseInt(st.nextToken());
+				int DD = Integer.parseInt(st.nextToken());
+				String YMD = String.format("%04d-%02d-%02d", YY, MM, DD);
+				try {
+					Date = new SimpleDateFormat("yyyy-MM-dd").parse(YMD);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} catch (IOException e) {
+				continue;
+			}
+			System.out.println("일정 내용을 입력 하시오.");
+			try {
+				schedule = br.readLine();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			cal.PlanMap(Date, schedule);
+			break;
+		}
+
+	}
+
+	public void cmdplan(BufferedReader br,Calender cal) {
+		Date Date = null;
+		System.out.println("년도와 달 날짜를 입력 하시오.");
+		while (true) {
+			try {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int YY = Integer.parseInt(st.nextToken());
+				int MM = Integer.parseInt(st.nextToken());
+				int DD = Integer.parseInt(st.nextToken());
+				String YMD = String.format("%04d-%02d-%02d", YY, MM, DD);
+				try {
+					Date = new SimpleDateFormat("yyyy-MM-dd").parse(YMD);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} catch (IOException e) {
+				continue;
+			}
+			cal.cmdPlanMap(Date);
+			break;
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
